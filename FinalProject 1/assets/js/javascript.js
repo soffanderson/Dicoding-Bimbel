2f6db985fe7fdfb3f6923f93f07e259b923846fb
soffanderson <s.btudz@gmail.com>
2022-07-24 07:26:10 +0800

    Kelar tinggal upload

--- FinalProject 1/assets/js/javascript.js
+++ FinalProject 1/assets/js/javascript.js
@@ -1,3 +1,4 @@
+// js nav-bar
 function myFunction() {
   let nav = document.getElementById("nav-container");
   if (nav.className === "nav-bar") {
@@ -7,6 +8,7 @@ function myFunction() {
   }
 }
 
+// js slider
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
@@ -34,6 +36,8 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+// js nama welcome
 const changeName = document.getElementById("nama");
 const firstName = prompt("Siapa namamu ?");
 
@@ -43,13 +47,31 @@ const user = {
 
 changeName.innerHTML = "<strong>" + user.name + ".</strong>";
 
-window.setTimeout("jam()", 1000);
+//js time
+// window.setTimeout("jam()", 1000);
 
-function jam() {
-  const now = new Date();
-  // const current = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-  // document.getElementById('jam').innerHTML = current;
-  setTimeout("jam()", 1000);
-  document.getElementById("jam").innerHTML = now.getHours();
-  document.getElementById("menit").innerHTML = now.getMinutes();
-}
+// function jam() {
+//   const now = new Date();
+//   // const current = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+//   // document.getElementById('jam').innerHTML = current;
+//   setTimeout("jam()", 1000);
+//   document.getElementById("jam").innerHTML = now.getHours();
+//   document.getElementById("menit").innerHTML = now.getMinutes();
+// }
+
+const currentTime = () => {
+  const el = document.getElementById("jam");
+
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+
+  hh = hh < 10 ? `0${hh}` : hh;
+  mm = mm < 10 ? `0${mm}` : mm;
+
+  let time = `${hh}:${mm}`;
+  el.innerHTML = time;
+};
+
+currentTime();
+setInterval(currentTime, 1000);
