70cd1c3e6b2f2461346fe52dd8c18e56a69b5ab0
soffanhdyt <s.btudz@gmail.com>
2022-08-16 23:27:53 +0800

    Smapai membuat Display Counter

---
+++ ReactJS/class-app/src/index.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function CounterDisplay({ count }) {
+  if (count === 0) {
+    return <p>{count}</p>;
+  }
+  if (count % 5 === 0 && count % 7 === 0) {
+    return <p>FizzBuzz</p>;
+  }
+  if (count % 5 === 0) {
+    return <p>Fizz</p>;
+  }
+
+  if (count % 7 === 0) {
+    return <p>Buzz</p>;
+  }
+
+  return <p>{count}</p>;
+}
+
+function IncreaseButton({ increase }) {
+  return (
+    <div>
+      <button onClick={increase}>+ increase</button>
+    </div>
+  );
+}
+
+function ResetButton({ reset }) {
+  return (
+    <div>
+      <button onClick={reset}>+ reset</button>
+    </div>
+  );
+}
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisai nilai count
+    this.state = {
+      count: 0,
+    };
+
+    // binding event handler
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+  }
+
+  onIncreaseEventHandler() {
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1,
+      };
+    });
+  }
+
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>TODO: selesaikan component-nya!</p>
+        <IncreaseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
+      </div>
+    );
+  }
+}
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     console.log("Component created!");
+//   }
+
+//   render() {
+//     return <p>Hello, {this.props.name}!</p>;
+//   }
+// }
+
+const root = createRoot(document.getElementById("root"));
+root.render(<CounterApp />);
