bcc0fe47d6159fdf429adefa9ba3198844a985a6
master~ <s.btudz@gmail.com>
2022-08-16 15:51:51 +0800

    sampai class components

--- ReactJS/contacts-app/src/utils/data.js
+++ ReactJS/contacts-app/src/utils/data.js
@@ -16,7 +16,7 @@ const getData = () => {
       id: 3,
       name: "Rahmat Fajri",
       tag: "rfajri127",
-      imageUrl: "/images/rfajri127.jpeg",
+      imageUrl: "/images/rfajri27.jpeg",
     },
   ];
 };
--- ReactJS/contacts-app/src/utils/index.js
+++ ReactJS/contacts-app/src/utils/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const element = <h1>Hello World</h1>;
+import ContactApp from "./components/ContactApp";
+import ".styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(element);
+root.render(<ContactApp />);
