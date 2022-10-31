cce5f9d784442a591fb723400a6d60371682732d
soffanderson <s.btudz@gmail.com>
2022-10-31 21:31:00 +0800

    Belajar framework Hapi

--- NodeJS/nodejs-web-server/routing.js
+++ NodeJS/nodejs-web-server/routing.js
@@ -9,17 +9,27 @@ const requestListener = (request, response) => {
     if (url === "/") {
         if (method === "GET") {
             response.statusCode = 200;
-            response.end("<h1>Ini adalah homepage.</h1>");
+            response.end(
+                JSON.stringify({
+                    message: "Ini adalah homepage.",
+                })
+            );
         } else {
             response.statusCode = 400;
             response.end(
-                `<h1>Halaman tidak dapat diakses dengan ${method} method.</h1>`
+                JSON.stringify({
+                    message: `Halaman tidak dapat diakses dengan ${method} method.`,
+                })
             );
         }
     } else if (url === "/about") {
         if (method === "GET") {
             response.statusCode = 200;
-            response.end("<h1>Ini adalah halaman about.</h1>");
+            response.end(
+                JSON.stringify({
+                    message: "Ini adalah halaman about.",
+                })
+            );
         } else if (method === "POST") {
             let body = [];
 
@@ -32,18 +42,26 @@ const requestListener = (request, response) => {
                 const { name } = JSON.parse(body);
                 response.statusCode = 200;
                 response.end(
-                    `<h1>Hello, ${name} ini adalah halaman about.</h1>`
+                    JSON.stringify({
+                        message: `Hello, ${name} ini adalah halaman about.`,
+                    })
                 );
             });
         } else {
             response.statusCode = 400;
             response.end(
-                `<h1>Halaman tidak dapat diakses dengan ${method} method.</h1>`
+                JSON.stringify({
+                    message: `Halaman tidak dapat diakses dengan ${method} method.`,
+                })
             );
         }
     } else {
         response.statusCode = 404;
-        response.end("<h1>Halaman tidak ditemukan!</h1>");
+        response.end(
+            JSON.stringify({
+                message: "Halaman tidak ditemukan.",
+            })
+        );
     }
 
     // response.statusCode = 200;
