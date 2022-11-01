c3ee17abb908efb6b65355cc0abb14351aa36141
soffanderson <s.btudz@gmail.com>
2022-11-01 23:00:24 +0800

    selesai request query

--- NodeJS/hepi-web-server/routes.js
+++ NodeJS/hepi-web-server/routes.js
@@ -28,6 +28,20 @@ const routes = [
         },
     },
     {
+        method: 'GET',
+        path: "/hello/{name?}",
+        handler: (request, h) => {
+            const {name = 'Stranger'} = request.params;
+            const {lang} = request.query;
+
+            if (lang === 'id') {
+                return `Hai, ${name}!`
+            }
+            
+            return `Hello, ${name}!`
+        }
+    },
+    {
         method: "*",
         path: "/{any*}",
         handler: (request, h) => {
