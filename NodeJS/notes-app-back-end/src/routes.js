a8784c4cf3a068a2f5a5b6b4d373900f48fdc91b
soffanderson <s.btudz@gmail.com>
2022-11-04 16:32:18 +0800

    Selesai pemabahsan node.js alhamdulillah

--- NodeJS/notes-app-back-end/src/routes.js
+++ NodeJS/notes-app-back-end/src/routes.js
@@ -2,6 +2,8 @@ const {
     addNoteHandler,
     getAllNotesHandler,
     getNoteByIdHandler,
+    editNotesByIdHandler,
+    deleteNotesByIdHandler,
 } = require("./handler");
 
 const router = [
@@ -19,6 +21,16 @@ const router = [
         method: "GET",
         path: "/notes/{id}",
         handler: getNoteByIdHandler,
+    },
+    {
+        method: "PUT",
+        path: "/notes/{id}",
+        handler: editNotesByIdHandler,
+    },
+    {
+        method: "DELETE",
+        path: "/notes/{id}",
+        handler: deleteNotesByIdHandler,
     },
 ];
 
