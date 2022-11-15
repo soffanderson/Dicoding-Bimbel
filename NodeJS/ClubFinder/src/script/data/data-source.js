2e3b82ee3b4c6a0f4a28da8b6e97c38e0a36f431
soffanderson <s.btudz@gmail.com>
2022-11-15 23:36:53 +0800

    Selesai pembahasan ES6 front-end

--- NodeJS/ClubFinder/src/script/data/data-source.js
+++ NodeJS/ClubFinder/src/script/data/data-source.js
@@ -1,18 +1,21 @@
+import clubs from './clubs.js';
+
 class DataSource {
-    constructor (onSuccess, onFailed) {
-        this.onSuccess = onSuccess;
-        this.onFailed = onFailed;
-    }
-    searchClub(keyword) {
-        const filteredClubs = clubs.filter((club) => {
-            return club.name.toUpperCase().includes(keyword.toUpperCase());
-        });
-    
-        if (filteredClubs.length) {
-            this.onSuccess(filteredClubs);
-        } else {
-            this.onFailed(`${keyword} is not found`);
-        }
-    }
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter((club) => {
+        return club.name
+          .toUpperCase()
+          .includes(keyword.toUpperCase());
+      });
+
+      if (filteredClubs.length) {
+        resolve(filteredClubs);
+      } else {
+        reject(`${keyword} is not found`);
+      }
+    });
+  }
 }
 
+export default DataSource;
