0d480d88d37372e4f6fe73d49c31d2be89bb29a8
soffanderson <s.btudz@gmail>
2022-11-29 22:21:22 +0800

    Selesai hingga rangkuman Webpack

--- FE/WebClock/webpack.common.js
+++ FE/WebClock/webpack.common.js
@@ -1,14 +1,14 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production',
   module: {
     rules: [
+      /* style and css loader */
       {
         test: /\.css$/,
         use: [
@@ -20,19 +20,14 @@ module.exports = {
           },
         ],
       },
-      /* babel loader */
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        ],
-      },
     ],
   },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+    }),
+  ],
 };
