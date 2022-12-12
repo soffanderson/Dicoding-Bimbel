7f02edf56ae81175d73dbfb777f5a716ff10af6d
soffanderson <s.btudz@gmail.com>
2022-12-12 16:34:52 +0800

    Selesai footer

--- FE/Submission-project-fe/src/script/component/app-bar.js
+++ FE/Submission-project-fe/src/script/component/app-bar.js
@@ -5,7 +5,7 @@ class AppBar extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <div class="max-w-5xl p-5 bg-slate-900 text-slate-200 mx-auto rounded-t-md shadow-slate-600 shadow-lg">
+    <div class="max-w-5xl p-5 bg-slate-900 text-slate-200 mx-auto rounded-t-md ">
       <h1 class="text-2xl text-center font-bold">Anime Collections</h1>
     </div>`;
   }
--- FE/Submission-project-fe/src/script/component/search-bar.js
+++ FE/Submission-project-fe/src/script/component/search-bar.js
@@ -1,28 +1,5 @@
 class SearchBar extends HTMLElement {
   connectedCallback() {
-    const getAnime = () => {
-      fetch(`https://gogoanime.consumet.org/popular`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((animelist) => {
-          if (animelist.error) {
-            showResponseMessage(animelist.message);
-          } else {
-            renderPopularAnime(animelist);
-          }
-        })
-        .catch((error) => {
-          showResponseMessage(error);
-        });
-    };
-
-    const showResponseMessage = (
-      message = 'Check your internet connection'
-    ) => {
-      alert(message);
-    };
-
     this.render();
   }
 
@@ -39,22 +16,33 @@ class SearchBar extends HTMLElement {
 
   render() {
     this.innerHTML = `
-      <form>
-      <div id="search-container" class="border-t flex flex-row-reverse border-slate-700 p-3 mx-auto max-w-5xl bg-slate-900">
-        <button
-        class="ml-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        id="searchButtonElement" type="submit">Search</button>
+      <div id="search-container" class="border-t grid grid-cols-2 border-slate-700 p-3 mx-auto max-w-5xl bg-slate-900">
+        <div class="flex items-center mx-auto">
+          <div class=" mt-0 mr-6 space-x-8 text-md font-bold text-slate-200">
+                  <ul class="flex flex-col mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 font-bold">
+        <li>
+          <a href="/" class="block py-2 pl-3 pr-4" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#recent" class="block py-2 pl-3 pr-4">Recent</a>
+        </li>
+        <li>
+          <a href="#populer" class="block py-2 pl-3 pr-4 ">Populer</a>
+        </li>
+      </ul>
+          </div>
+      </div>
+  
+      <div class="flex">
         <input
         class="max-w-md bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Search anime" id="searchElement" type="search">
-        <div class="flex items-center mx-auto">
-          <div class="flex flex-row mt-0 mr-6 space-x-8 text-md font-bold text-slate-200">
-            Today Selection: 
-          </div>
+        <button
+        class="ml-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        id="searchButtonElement" type="submit">Search</button>
+      </div>  
       </div>
-      </div>
-      </form>
-      `;
+    `;
 
     this.querySelector('#searchButtonElement').addEventListener(
       'click',
