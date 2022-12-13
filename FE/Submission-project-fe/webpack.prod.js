02f9c61f87e2cef1c55d1e63369bafe791c73638
soffanderson <s.btudz@gmail.com>
2022-12-13 23:59:20 +0800

    Selesai menambahkan plugin

--- FE/Submission-project-fe/webpack.prod.js
+++ FE/Submission-project-fe/webpack.prod.js
@@ -1,5 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const path = require('path')
+const glob = require('glob')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
+
+
 module.exports = merge(common, {
   mode: 'production',
   module: {
@@ -15,7 +24,17 @@ module.exports = merge(common, {
             },
           },
         ],
+                test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
       },
     ],
   },
+  plugins: [
+        new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ]
 });
