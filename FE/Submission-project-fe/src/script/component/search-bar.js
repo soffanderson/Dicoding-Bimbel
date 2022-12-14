66cace5070009c1703f19b03c602254beb212903
soffanderson <s.btudz@gmail.com>
2022-12-14 17:17:04 +0800

    Masih berkutat dengan nav responsive

--- FE/Submission-project-fe/src/script/component/search-bar.js
+++ FE/Submission-project-fe/src/script/component/search-bar.js
@@ -14,31 +14,39 @@ class SearchBar extends HTMLElement {
 
 	render() {
 		this.innerHTML = `
-      <div id="search-container" class="border-t grid grid-cols-2 border-slate-700 p-3 mx-auto max-w-5xl bg-slate-900">
-          <div class="flex items-center mx-auto">
-              <div class=" mt-0 mr-6 space-x-8 text-md font-bold text-slate-200">
-                  <ul class="flex flex-col mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 font-bold">
-                      <li>
-                          <a href="/" class="block py-2 pl-3 pr-4" aria-current="page">Home</a>
-                      </li>
-                      <li>
-                          <a href="#recent" class="block py-2 pl-3 pr-4">Recent</a>
-                      </li>
-                      <li>
-                          <a href="#populer" class="block py-2 pl-3 pr-4 ">Populer</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
+<nav id="search-container" class="flex items-center justify-between flex-wrap bg-slate-900 border-t border-slate-700 p-2">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Docs
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Examples
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Blog
+      </a>
+    </div>
 
-          <div class="flex">
-              <input
-                  class="max-w-md bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search anime" id="searchElement" type="search">
-              <button
-                  class="ml-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  id="searchButtonElement" type="submit">Search</button>
-          </div>
+    <div class="flex p-2 md:p-1">
+        <input
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search anime" id="searchElement" type="search">
+        <button
+            class="ml-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            id="searchButtonElement" type="submit">Search</button>
+    </div>
+  </div>
+</nav>
       </div>
     `;
 
