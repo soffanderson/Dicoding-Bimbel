d474c072061a81149f09baeaa5f340fdafb17f56
soffanderson <s.btudz@gmail.com>
2023-02-02 14:59:19 +0800

    selesai membuat navbar aksibilitas

--- FE/7. restaurant-apps/src/scripts/index.js
+++ FE/7. restaurant-apps/src/scripts/index.js
@@ -3,20 +3,33 @@ import '../styles/main.css';
 
 console.log('Hello Coders! :)');
 
-const menu = document.querySelector('#openMenu');
 const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
+const checkbox = document.querySelector('#openMenu');
 
-menu.addEventListener('click', function (event) {
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode == 32 || e.keyCode == 13) {
+    drawer.classList.toggle('open');
+    if (checkbox.checked == true) {
+      checkbox.checked = false;
+    } else {
+      checkbox.checked = true;
+    }
+  }
+});
+
+checkbox.addEventListener('click', function () {
   drawer.classList.toggle('open');
   event.stopPropagation();
 });
 
 hero.addEventListener('click', function () {
   drawer.classList.remove('open');
+  checkbox.checked = false;
 });
 
 main.addEventListener('click', function () {
   drawer.classList.remove('open');
+  checkbox.checked = false;
 });
