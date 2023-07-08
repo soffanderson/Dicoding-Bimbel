422721312bb278cea8d2ebd018cf72acd1a8c89f
soffanderson <s.btudz@gmail.com>
2023-07-08 21:13:02 +0800

    Kau Tak Perlu Tahu Rahasiaku

--- FE/12. movie-catalogue TDD/specs/favoriteMovieSearchSpec.js
+++ FE/12. movie-catalogue TDD/specs/favoriteMovieSearchSpec.js
@@ -44,64 +44,6 @@ describe('Searching movies', () => {
       expect(favoriteMovies.searchMovies).toHaveBeenCalledWith('film a');
     });
 
-    it('should show the found movies', () => {
-      presenter._showFoundMovies([{ id: 1 }]);
-      expect(document.querySelectorAll('.movie').length).toEqual(1);
-
-      presenter._showFoundMovies([
-        {
-          id: 1,
-          title: 'Satu',
-        },
-        {
-          id: 2,
-          title: 'Dua',
-        },
-      ]);
-      expect(document.querySelectorAll('.movie').length).toEqual(2);
-    });
-
-    it('should show the title of the found movies', () => {
-      presenter._showFoundMovies([
-        {
-          id: 1,
-          title: 'Satu',
-        },
-      ]);
-      expect(document.querySelectorAll('.movie__title').item(0).textContent).toEqual('Satu');
-    });
-
-    it('should show the title of the found movies', () => {
-      presenter._showFoundMovies([
-        {
-          id: 1,
-          title: 'Satu',
-        },
-      ]);
-      expect(document.querySelectorAll('.movie__title').item(0).textContent).toEqual('Satu');
-
-      presenter._showFoundMovies([
-        {
-          id: 1,
-          title: 'Satu',
-        },
-        {
-          id: 2,
-          title: 'Dua',
-        },
-      ]);
-
-      const movieTitles = document.querySelectorAll('.movie__title');
-      expect(movieTitles.item(0).textContent).toEqual('Satu');
-      expect(movieTitles.item(1).textContent).toEqual('Dua');
-    });
-
-    it('should show - for found movie without title', () => {
-      presenter._showFoundMovies([{ id: 1 }]);
-
-      expect(document.querySelectorAll('.movie__title').item(0).textContent).toEqual('-');
-    });
-
     it('should show the movies found by Favorite Movies', (done) => {
       document.getElementById('movie-search-container').addEventListener('movies:searched:updated', () => {
         expect(document.querySelectorAll('.movie').length).toEqual(3);
@@ -150,6 +92,19 @@ describe('Searching movies', () => {
           title: 'ini juga boleh film a',
         },
       ]);
+
+      searchMovies('film a');
+    });
+
+    it('should show - when the movie returned does not contain a title', (done) => {
+      document.getElementById('movie-search-container').addEventListener('movies:searched:updated', () => {
+        const movieTitles = document.querySelectorAll('.movie__title');
+        expect(movieTitles.item(0).textContent).toEqual('-');
+
+        done();
+      });
+
+      favoriteMovies.searchMovies.withArgs('film a').and.returnValues([{ id: 444 }]);
 
       searchMovies('film a');
     });
